51d90d548771a19a345ea1130992e8e558640b85
Denys <denys.x95@gmail.com>
2026-03-18 01:45:47 +0200

    made migrations

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {'default': env.db()}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
