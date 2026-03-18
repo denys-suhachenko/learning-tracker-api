65dfd35fa079642c13a9dbbfc33cab348d115d2e
Denys <denys.x95@gmail.com>
2026-03-18 03:45:46 +0200

    hotfix: defaul permissions for auth

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 }
 
 SIMPLE_JWT = {
